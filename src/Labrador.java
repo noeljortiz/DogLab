da8e18261e380bf69389890cd0ec3abc7d68f13d
noeljortiz <noelo12130613@gmail.com>
2023-02-01 09:28:04 -0600

    initial

---
+++ src/Labrador.java
@@ -0,0 +1,30 @@
+// ****************************************************************
+// Labrador.java
+//
+// A class derived from Dog that holds information about
+// a labrador retriever.  Overrides Dog speak method and includes
+// information about avg weight for this breed.
+//
+// ****************************************************************
+
+
+public class Labrador extends Dog
+{
+    private String color; //black, yellow, or chocolate?
+
+
+    public Labrador(String name,  String color)
+    {
+        super();
+        this.color = color;
+    }
+
+
+    public String bark()
+    {
+        return "Labrador barking";
+    }
+
+
+
+}
